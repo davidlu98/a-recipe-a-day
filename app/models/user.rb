f6ebcbb071a2f63276ff300a5a4407e45975091d
Donald <tek.duy@gmail.com>
2018-02-13 21:59:36 -0800

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ApplicationRecord
-  attr_accessor :email, :name, :password, :password_confirmation
+  attr_accessor :password, :password_confirmation, :encrypted_password
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+
+  before_save :encrypt_password
+
+  require 'digest/sha1'
+
+  def encrypt_password
+    if password.present?
+      self.encrypted_password = Digest::SHA1.hexdigest(password)
+    end
+  end
+
+  def authenticate(password)
+    encrypted_password == Digest::SHA1.hexdigest(password)
+  end
 
   validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213215233) do
+ActiveRecord::Schema.define(version: 20180213231006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20180213215233) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
+    t.string "encrypted_password"
+    t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
