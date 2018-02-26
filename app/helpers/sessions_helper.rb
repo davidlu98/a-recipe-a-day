46b8d4d5adc0fc03b4abd0b02eeeadf11fd477e6
Donald <tek.duy@gmail.com>
2018-02-25 21:30:00 -0800

    fixed settings

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
