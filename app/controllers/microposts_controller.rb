49d127166432794d01bdadeeba14f2a7c251a1fb
donald <tek.duy@gmail.com>
2018-03-05 21:40:42 -0800

    admins can now delete posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
+  before_action :admin_user, only: :destroy
 
   def new
     @micropost = Micropost.new
@@ -32,5 +33,9 @@ class MicropostsController < ApplicationController
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
