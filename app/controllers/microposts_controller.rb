5b947f46c73c1859aab7dbf252d91c2f15f12b27
tekduy <36016701+tekduy@users.noreply.github.com>
2018-03-28 19:23:47 -0700

    Update microposts_controller.rb
    Fixed users not being able to delete microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
-  before_action :admin_user, only: :destroy
+  #before_action :admin_user, only: :destroy
 
   def new
     @micropost = Micropost.new
